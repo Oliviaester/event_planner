aeeb04de392ca17ee60fe505e04c7aba94bdf58e
Ubuntu <ubuntu@ip-172-31-31-128.us-east-2.compute.internal>
2019-03-27 23:26:47 +0000

    updating repo from deploy

--- event_planner/settings.py
+++ event_planner/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k!m9sv0_y*kl5)kc!&#m#(jhm!+x4m7cx)tm+m&$*g50d%$9ke'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.220.248.217']
 
 
 # Application definition
@@ -120,5 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'MEDIA')
